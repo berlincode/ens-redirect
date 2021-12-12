5a723a420f23b145afe47bd56600df274d6f775c
elastic <elastic.code@gmail.com>
2021-12-12 10:45:14 +0100

    refactor javascript

--- js/client.js
+++ js/client.js
@@ -6,39 +6,152 @@ import '../css/base.scss';
 
 import ethEnsNamehash from 'eth-ens-namehash';
 
-(function(){
+const removeDomainLevels = 2;
 
-  function console_log(msg){
-    if (window.console){
-      window.console.log(msg);
+const ipfsBaseUrl = 'https://ipfs.infura.io/ipfs/';
+
+/* mainnet */
+const rpcHost = 'https://mainnet.infura.io/v3/<PROJECT_ID>'; // enter here a valid infura PROJECT_ID
+const registry = '0x314159265dd8dbb310642f98f50c066173c1259b'; // mainnet
+
+/* ropsten */
+//const rpcHost = 'https://ropsten.infura.io/v3/<PROJECT_ID>'; // enter here a valid infura PROJECT_ID
+//const registry = '0x112234455c3a32fd11230c42e7bccd4a84e02010'; // ropsten
+
+
+function console_log(msg){
+  if (window.console){
+    window.console.log(msg);
+  }
+}
+
+function removePrefix(hex){
+  if (hex.substr(0, 2) === '0x')
+    return hex.substr(2);
+  return hex;
+}
+
+function requestPostJson(url, data, done){
+  const xhr = new XMLHttpRequest(); // new HttpRequest instance
+  xhr.onreadystatechange = function() {
+    if (this.readyState == 4){
+      let response;
+      if (this.status == 200) {
+        try {
+          response = JSON.parse(this.responseText);
+        } catch (err){
+          done(err);
+          return;
+        }
+        const result = response.result;
+
+        if (! result){
+          done('Geth request failed');
+          return;
+        }
+
+        if (result.length <= 2){
+          done('Invalid response');
+          return;
+        }
+
+        done(null, result);
+      } else {
+        try {
+          response = JSON.parse(this.responseText);
+        } catch (err){
+          //
+        }
+        if (response.error && response.error.message){
+          done(response.error.message);
+          return;
+        }
+        done('retured status code ' + this.status);
+      }
     }
-  }
+  };
+  xhr.onerror = function () {
+    done(xhr.response);
+  };
+  xhr.open('POST', url);
+  xhr.setRequestHeader('Content-Type', 'application/json;charset=utf-8');
+  xhr.send(JSON.stringify(data));
+}
 
-  var removeDomainLevels = 2;
+function lookupResolver(host, nameHash, registry, done){
+  const dataGetResolver = {
+    'id': 0,
+    'jsonrpc': '2.0',
+    'params': [
+      {
+        'to': registry,
+        // '0x0178b8bf' = 'resolver(bytes32)' (see https://www.4byte.directory/)
+        'data': '0x0178b8bf' + removePrefix(nameHash)
+      },
+      'latest'
+    ],
+    'method': 'eth_call'
+  };
+  requestPostJson(host, dataGetResolver, function(err, result){
+    if (err){
+      done(err);
+      return;
+    }
+    result = removePrefix(result);
+    const resolver = '0x' + result.substr(24);
+    done((resolver === '0x0000000000000000000000000000000000000000')? 'invalid resolver' : null, resolver);
+  });
+}
 
-  var ipfsBaseUrl = 'https://ipfs.infura.io/ipfs/';
+function lookupContenthash(host, nameHash, resolver, done){
 
-  /* mainnet */
-  var rpcHost = 'https://mainnet.infura.io/v3/<PROJECT_ID>';
-  var registry = '0x314159265dd8dbb310642f98f50c066173c1259b'; // mainnet
+  const dataGetContentHash = {
+    'id': 1,
+    'jsonrpc': '2.0',
+    'params': [
+      {
+        'to': resolver,
+        // "0x3b3b57de" = "addr(bytes32)" (see https://www.4byte.directory/) // works for ethereum.eth (resolver: 0x1da022710df5002339274aadee8d58218e9d6ab5)
+        // "0x2dff6941" = "content(bytes32)" (see https://www.4byte.directory/) // works for portalnetwork.eth (resolver: 0x1da022710df5002339274aadee8d58218e9d6ab5)
+        // "0xbc1c58d1" = "contenthash(bytes32)"
+        'data': '0xbc1c58d1' + removePrefix(nameHash)
+      },
+      'latest'
+    ],
+    'method': 'eth_call'
+  };
+  requestPostJson(host, dataGetContentHash, function(err, result){
+    if (err){
+      done(err);
+      return;
+    }
+    result = removePrefix(result);
+    let contentHash = result.substr(32); // TODO
+    contentHash = contentHash.substr(32); // TODO
+    const length = contentHash.substr(0, 64); // TODO
+    const lengthInt = parseInt(length, 16);
+    if (lengthInt === 0){
+      done('invalid length');
+      return;
+    }
+    contentHash = contentHash.substr(64).substr(0, lengthInt*2); // TODO
+    done(null, contentHash);
+  });
+}
 
-  /* ropsten */
-  //var rpcHost = 'https://ropsten.infura.io/v3/<PROJECT_ID>';
-  //var registry = '0x112234455c3a32fd11230c42e7bccd4a84e02010'; // ropsten
 
-  /* get the hostname from location.hostname by remoming the last parts and by adding '.eth' */
-  var hostname = window.location.hostname;
-  var hostnameParts = hostname.split('.');
-  for (var i=0; i < removeDomainLevels ; i++){
-    hostnameParts.pop();
-  }
-  var hostnameENS = hostnameParts.join('.') + '.eth';
+function contenthashToCID(contenthash){
+  // first byte should be 'e3' for ipfs, then '01' - add 'f' to sign hex codes cid
+  return 'f' + contenthash.substr(4);
+}
+
+function redirect(hostnameENS){
 
   /* show which domain we are resolving */
   document.getElementById('d-text').textContent = 'Redirect to "' + hostnameENS + '"';
 
   /* now calculate the hash of the domain which is then send to our ethereum rpc provider */
-  var nameHash = ethEnsNamehash.hash(hostnameENS);
+  const nameHash = ethEnsNamehash.hash(hostnameENS);
 
   /*
     dots:
@@ -46,23 +159,20 @@ import ethEnsNamehash from 'eth-ens-namehash';
         #1: lookupResolver
         #2: lookupContenthash
   */
-  var dotShow = function(){
+  function dotShow(){
     document.getElementById('d-dot').style.display = 'block';
-  };
-  var errorDisplayed = null;
-  var dotSet = function(dotNr, error){
+  }
+  function dotSet(dotNr, error){
     document.getElementById('d-dot' + dotNr).setAttribute('fill', error? '#ff0000' : '#b3e0f2');
-    if (error && (! errorDisplayed)){
-      // we are only displaying the first error
-      errorDisplayed = error;
-      var text = document.getElementById('d-text');
-      text.textContent = errorDisplayed;
+    if (error){
+      const text = document.getElementById('d-text');
+      text.textContent = error;
       text.setAttribute('fill', '#ff0000');
     }
-  };
-  var dotCurrent = 0;
-  var dotState = [];
-  var dotSetInOrder = function(dotNr, error){
+  }
+  let dotCurrent = 0;
+  let dotState = [];
+  function dotSetInOrder(dotNr, error){
     dotState[dotNr] = error;
 
     while (typeof(dotState[dotCurrent]) !== 'undefined'){
@@ -74,130 +184,17 @@ import ethEnsNamehash from 'eth-ens-namehash';
       if (error){
         // show input field
         document.getElementById('input').style.display = 'block';
+        document.getElementById('input-submit').disabled = 'disabled';
+        document.getElementById('input-input').disabled = 'disabled';
+
+        //document.getElementById('input-submit').disabled = false;
+        //document.getElementById('input-input').disabled = false;
 
         console_log(error);
         //sendError('Error: '+ error);
       }
     } catch (e){/*empty*/}
-  };
-
-  var removePrefix = function(hex){
-    if (hex.substr(0, 2) === '0x')
-      return hex.substr(2);
-    return hex;
-  };
-
-  var requestPostJson = function(url, data, done){
-    var xhr = new XMLHttpRequest(); // new HttpRequest instance
-    xhr.onreadystatechange = function() {
-      if (this.readyState == 4){
-        var response;
-        if (this.status == 200) {
-          try {
-            response = JSON.parse(this.responseText);
-          } catch (err){
-            done(err);
-            return;
-          }
-          var result = response.result;
-
-          if (! result){
-            done('Geth request failed');
-            return;
-          }
-
-          if (result.length <= 2){
-            done('Invalid response');
-            return;
-          }
-
-          done(null, result);
-        } else {
-          try {
-            response = JSON.parse(this.responseText);
-          } catch (err){
-            //
-          }
-          if (response.error && response.error.message){
-            done(response.error.message);
-            return;
-          }
-          done('retured status code ' + this.status);
-        }
-      }
-    };
-    xhr.onerror = function () {
-      done(xhr.response);
-    };
-    xhr.open('POST', url);
-    xhr.setRequestHeader('Content-Type', 'application/json;charset=utf-8');
-    xhr.send(JSON.stringify(data));
-  };
-
-  var lookupResolver = function(host, nameHash, registry, done){
-    var dataGetResolver = {
-      'id': 0,
-      'jsonrpc': '2.0',
-      'params': [
-        {
-          'to': registry,
-          // '0x0178b8bf' = 'resolver(bytes32)' (see https://www.4byte.directory/)
-          'data': '0x0178b8bf' + removePrefix(nameHash)
-        },
-        'latest'
-      ],
-      'method': 'eth_call'
-    };
-    requestPostJson(host, dataGetResolver, function(err, result){
-      if (err){
-        done(err);
-        return;
-      }
-      result = removePrefix(result);
-      var resolver = '0x' + result.substr(24);
-      done((resolver === '0x0000000000000000000000000000000000000000')? 'invalid resolver' : null, resolver);
-    });
-  };
-
-  var lookupContenthash = function(host, nameHash, resolver, done){
-
-    var dataGetContentHash = {
-      'id': 1,
-      'jsonrpc': '2.0',
-      'params': [
-        {
-          'to': resolver,
-          // "0x3b3b57de" = "addr(bytes32)" (see https://www.4byte.directory/) // works for ethereum.eth (resolver: 0x1da022710df5002339274aadee8d58218e9d6ab5)
-          // "0x2dff6941" = "content(bytes32)" (see https://www.4byte.directory/) // works for portalnetwork.eth (resolver: 0x1da022710df5002339274aadee8d58218e9d6ab5)
-          // "0xbc1c58d1" = "contenthash(bytes32)"
-          'data': '0xbc1c58d1' + removePrefix(nameHash)
-        },
-        'latest'
-      ],
-      'method': 'eth_call'
-    };
-    requestPostJson(host, dataGetContentHash, function(err, result){
-      if (err){
-        done(err);
-        return;
-      }
-      result = removePrefix(result);
-      var contentHash = result.substr(32); // TODO
-      contentHash = contentHash.substr(32); // TODO
-      var length = contentHash.substr(0, 64); // TODO
-      var lengthInt = parseInt(length, 16);
-      if (lengthInt === 0){
-        done('invalid length');
-        return;
-      }
-      contentHash = contentHash.substr(64).substr(0, lengthInt*2); // TODO
-      done(null, contentHash);
-    });
-  };
-  var contenthashToCID = function(contenthash){
-    // first byte should be 'e3' for ipfs, then '01' - add 'f' to sign hex codes cid
-    return 'f' + contenthash.substr(4);
-  };
+  }
 
   dotShow(); // signal that js started to execute
 
@@ -221,8 +218,18 @@ import ethEnsNamehash from 'eth-ens-namehash';
         console_log(err);
         return;
       }
-      var cid = contenthashToCID(contenthash);
+      const cid = contenthashToCID(contenthash);
       window.location.replace(ipfsBaseUrl + cid + location.pathname);
     });
   });
-})();
+}
+
+/* get the hostname from location.hostname by remoming the last parts and by adding '.eth' */
+const hostname = window.location.hostname;
+const hostnameParts = hostname.split('.');
+for (let i=0; i < removeDomainLevels ; i++){
+  hostnameParts.pop();
+}
+const hostnameENS = hostnameParts.join('.') + '.eth';
+redirect(hostnameENS);
+
